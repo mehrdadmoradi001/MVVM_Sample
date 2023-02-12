ef6ecf1e1fca3d2bf8a317c2750a672ecebe2049
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-02-12 10:27:54 +0330

    1- creating coinList_screen.dart

---
+++ lib/view/coinList_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm/view_model/coinlist_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+class CoinListScreen extends StatefulWidget {
+  const CoinListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CoinListScreen> createState() => _CoinListScreenState();
+}
+
+class _CoinListScreenState extends State<CoinListScreen> {
+  final _viewModel = CoinListViewModel();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _viewModel.loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => _viewModel,
+      child: Consumer<CoinListViewModel>(
+        builder: ((context, viewModel, child) => _getUi(viewModel)),
+      ),
+    );
+  }
+
+  Widget _getUi(CoinListViewModel viewModel) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: viewModel.cryptoList.length ?? 0,
+        itemBuilder: (context, index) {
+          return Text(viewModel.cryptoList[index].name ?? '');
+        },
+      ),
+    );
+  }
+}
